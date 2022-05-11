f739cf7bc7fb35d0feebcea2416a1e76855a1367
Sahil Munjhal <95855396+SahilMunjhal@users.noreply.github.com>
2022-05-12 00:53:59 +0530

    navbar-ready

---
+++ Front-End/e-commerce-website/src/Components/Navbar/navbar.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { NavbarMenu } from "../NavbarMenu/navbarmenu";
+
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import "./navbar.css";
+
+export const Navbar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    "& .MuiBadge-badge": {
+      right: -3,
+      top: 13,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: "0 4px",
+    },
+  }));
+
+  return (
+    <div className="navbar-header">
+      <img
+        className="navbar-image"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxCFBaj928OdJ7RTEi8gJFPvHphBLxZv0fpA&usqp=CAU"
+      />
+
+      <NavbarMenu />
+      <div className="navbar-search">
+        <input type="text" placeholder="search" />
+      </div>
+      <div className="navbar-right">
+        <div>
+          <PermIdentityIcon fontSize="medium" />
+          <div className="ProfileIcon">
+            <Button
+              id="basicButton"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              Profile
+            </Button>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={handleClose}>My account</MenuItem>
+              <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+          </div>
+        </div>
+        <div>
+          <FavoriteBorderIcon />
+          <h3 className="navbar-wishlist">WishList</h3>
+        </div>
+        <div className="navbar-bag">
+          <IconButton aria-label="cart">
+            <StyledBadge badgeContent={4} color="secondary">
+              <ShoppingCartIcon />
+            </StyledBadge>
+          </IconButton>
+          <h3>Bag</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ Front-End/e-commerce-website/src/Components/NavbarMenu/navbarmenu.jsx
@@ -0,0 +1,482 @@
+import "./navbarmenu.css";
+
+export const NavbarMenu = () => {
+  return (
+    <div className="navbarmenu">
+      <ul>
+        <li>
+          Mens
+          <div className="navbar-menu-mens">
+            <div className="navbar-menu-mens-two">
+            <div>
+              <h3>TopWear</h3>
+              <ul>
+                <li>T-Shirts</li>
+                <li>Casual-Shirts</li>
+                <li>Formal-Shirt</li>
+                <li>SweatShirts</li>
+                <li>Sweater</li>
+                <li>Jackets</li>
+                <li>Blazer & Coats</li>
+                <li>RainCoats</li>
+              </ul>
+            </div>
+            <div>
+              <h3>Indian and Festive year</h3>
+              <ul>
+                <li>Kurta & Kurtas Set</li>
+                <li>Sherwanis</li>
+                <li>Nehru Jackets</li>
+                <li>Dhotis</li>
+              </ul>
+            </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>BottomWear</h3>
+                <ul>
+                  <li>Jeans</li>
+                  <li>Formal Trouser</li>
+                  <li>Casual Trouser</li>
+                  <li>Shorts</li>
+                  <li>TrackPants & Trousers</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Innerwear & SleepWear</h3>
+                <ul>
+                  <li>Brief & Thunks</li>
+                  <li>Boxers</li>
+                  <li>Vests</li>
+                  <li>SleepWear & LongeWear</li>
+                  <li>Thermals</li>
+                </ul>
+              </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>Shoes</h3>
+                <ul>
+                  <li>Casual-Shoes</li>
+                  <li>Formal Shoes</li>
+                  <li>Sports Shoes</li>
+                  <li>Sneakers</li>
+                  <li>Sandals & Loaters </li>
+                  <li>Flip-Flops</li>
+                  <li>Socks</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Personal-Care & Groomings</h3>
+                <h3>Sunglasses & Frames</h3>
+                <h3>Watches</h3>
+              </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>Sports and Active-Wear</h3>
+                <ul>
+                  <li>Casual-Shoes</li>
+                  <li>Formal Shoes</li>
+                  <li>Sports Shoes</li>
+                  <li>Sneakers</li>
+                  <li>Sandals & Loaters </li>
+                  <li>Flip-Flops</li>
+                  <li>Jackets & SweatShirts</li>
+                  <li>Sports-Activewear</li>
+                  <li>Swimwear</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Gadgets</h3>
+                <ul>
+                 <li>Smart Wearable</li>
+                  <li>Fitness Gadgets</li>
+                  <li>HeadPhones</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>Womens
+        <div className="navbar-menu-women">
+            <div className="navbar-menu-mens-two">
+            <div>
+              <h3>TopWear</h3>
+              <ul>
+                <li>T-Shirts</li>
+                <li>Casual-Shirts</li>
+                <li>Formal-Shirt</li>
+                <li>SweatShirts</li>
+                <li>Sweater</li>
+                <li>Jackets</li>
+                <li>Blazer & Coats</li>
+                <li>RainCoats</li>
+              </ul>
+            </div>
+            <div>
+              <h3>Indian and Festive year</h3>
+              <ul>
+                <li>Kurta & Kurtas Set</li>
+                <li>Sherwanis</li>
+                <li>Nehru Jackets</li>
+                <li>Dhotis</li>
+              </ul>
+            </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>BottomWear</h3>
+                <ul>
+                  <li>Jeans</li>
+                  <li>Formal Trouser</li>
+                  <li>Casual Trouser</li>
+                  <li>Shorts</li>
+                  <li>TrackPants & Trousers</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Innerwear & SleepWear</h3>
+                <ul>
+                  <li>Brief & Thunks</li>
+                  <li>Boxers</li>
+                  <li>Vests</li>
+                  <li>SleepWear & LongeWear</li>
+                  <li>Thermals</li>
+                </ul>
+              </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>Shoes</h3>
+                <ul>
+                  <li>Casual-Shoes</li>
+                  <li>Formal Shoes</li>
+                  <li>Sports Shoes</li>
+                  <li>Sneakers</li>
+                  <li>Sandals & Loaters </li>
+                  <li>Flip-Flops</li>
+                  <li>Socks</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Personal-Care & Groomings</h3>
+                <h3>Sunglasses & Frames</h3>
+                <h3>Watches</h3>
+              </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>Sports and Active-Wear</h3>
+                <ul>
+                  <li>Casual-Shoes</li>
+                  <li>Formal Shoes</li>
+                  <li>Sports Shoes</li>
+                  <li>Sneakers</li>
+                  <li>Sandals & Loaters </li>
+                  <li>Flip-Flops</li>
+                  <li>Jackets & SweatShirts</li>
+                  <li>Sports-Activewear</li>
+                  <li>Swimwear</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Gadgets</h3>
+                <ul>
+                 <li>Smart Wearable</li>
+                  <li>Fitness Gadgets</li>
+                  <li>HeadPhones</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>Kids
+        <div className="navbar-menu-kids">
+            <div className="navbar-menu-mens-two">
+            <div>
+              <h3>TopWear</h3>
+              <ul>
+                <li>T-Shirts</li>
+                <li>Casual-Shirts</li>
+                <li>Formal-Shirt</li>
+                <li>SweatShirts</li>
+                <li>Sweater</li>
+                <li>Jackets</li>
+                <li>Blazer & Coats</li>
+                <li>RainCoats</li>
+              </ul>
+            </div>
+            <div>
+              <h3>Indian and Festive year</h3>
+              <ul>
+                <li>Kurta & Kurtas Set</li>
+                <li>Sherwanis</li>
+                <li>Nehru Jackets</li>
+                <li>Dhotis</li>
+              </ul>
+            </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>BottomWear</h3>
+                <ul>
+                  <li>Jeans</li>
+                  <li>Formal Trouser</li>
+                  <li>Casual Trouser</li>
+                  <li>Shorts</li>
+                  <li>TrackPants & Trousers</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Innerwear & SleepWear</h3>
+                <ul>
+                  <li>Brief & Thunks</li>
+                  <li>Boxers</li>
+                  <li>Vests</li>
+                  <li>SleepWear & LongeWear</li>
+                  <li>Thermals</li>
+                </ul>
+              </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>Shoes</h3>
+                <ul>
+                  <li>Casual-Shoes</li>
+                  <li>Formal Shoes</li>
+                  <li>Sports Shoes</li>
+                  <li>Sneakers</li>
+                  <li>Sandals & Loaters </li>
+                  <li>Flip-Flops</li>
+                  <li>Socks</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Personal-Care & Groomings</h3>
+                <h3>Sunglasses & Frames</h3>
+                <h3>Watches</h3>
+              </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>Sports and Active-Wear</h3>
+                <ul>
+                  <li>Casual-Shoes</li>
+                  <li>Formal Shoes</li>
+                  <li>Sports Shoes</li>
+                  <li>Sneakers</li>
+                  <li>Sandals & Loaters </li>
+                  <li>Flip-Flops</li>
+                  <li>Jackets & SweatShirts</li>
+                  <li>Sports-Activewear</li>
+                  <li>Swimwear</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Gadgets</h3>
+                <ul>
+                 <li>Smart Wearable</li>
+                  <li>Fitness Gadgets</li>
+                  <li>HeadPhones</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>Kids
+        <div className="navbar-menu-kids">
+            <div className="navbar-menu-mens-two">
+            <div>
+              <h3>TopWear</h3>
+              <ul>
+                <li>T-Shirts</li>
+                <li>Casual-Shirts</li>
+                <li>Formal-Shirt</li>
+                <li>SweatShirts</li>
+                <li>Sweater</li>
+                <li>Jackets</li>
+                <li>Blazer & Coats</li>
+                <li>RainCoats</li>
+              </ul>
+            </div>
+            <div>
+              <h3>Indian and Festive year</h3>
+              <ul>
+                <li>Kurta & Kurtas Set</li>
+                <li>Sherwanis</li>
+                <li>Nehru Jackets</li>
+                <li>Dhotis</li>
+              </ul>
+            </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>BottomWear</h3>
+                <ul>
+                  <li>Jeans</li>
+                  <li>Formal Trouser</li>
+                  <li>Casual Trouser</li>
+                  <li>Shorts</li>
+                  <li>TrackPants & Trousers</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Innerwear & SleepWear</h3>
+                <ul>
+                  <li>Brief & Thunks</li>
+                  <li>Boxers</li>
+                  <li>Vests</li>
+                  <li>SleepWear & LongeWear</li>
+                  <li>Thermals</li>
+                </ul>
+              </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>Shoes</h3>
+                <ul>
+                  <li>Casual-Shoes</li>
+                  <li>Formal Shoes</li>
+                  <li>Sports Shoes</li>
+                  <li>Sneakers</li>
+                  <li>Sandals & Loaters </li>
+                  <li>Flip-Flops</li>
+                  <li>Socks</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Personal-Care & Groomings</h3>
+                <h3>Sunglasses & Frames</h3>
+                <h3>Watches</h3>
+              </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>Sports and Active-Wear</h3>
+                <ul>
+                  <li>Casual-Shoes</li>
+                  <li>Formal Shoes</li>
+                  <li>Sports Shoes</li>
+                  <li>Sneakers</li>
+                  <li>Sandals & Loaters </li>
+                  <li>Flip-Flops</li>
+                  <li>Jackets & SweatShirts</li>
+                  <li>Sports-Activewear</li>
+                  <li>Swimwear</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Gadgets</h3>
+                <ul>
+                 <li>Smart Wearable</li>
+                  <li>Fitness Gadgets</li>
+                  <li>HeadPhones</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>Human & Living
+        <div className="navbar-menu-living">
+            <div className="navbar-menu-mens-two">
+            <div>
+              <h3>TopWear</h3>
+              <ul>
+                <li>T-Shirts</li>
+                <li>Casual-Shirts</li>
+                <li>Formal-Shirt</li>
+                <li>SweatShirts</li>
+                <li>Sweater</li>
+                <li>Jackets</li>
+                <li>Blazer & Coats</li>
+                <li>RainCoats</li>
+              </ul>
+            </div>
+            <div>
+              <h3>Indian and Festive year</h3>
+              <ul>
+                <li>Kurta & Kurtas Set</li>
+                <li>Sherwanis</li>
+                <li>Nehru Jackets</li>
+                <li>Dhotis</li>
+              </ul>
+            </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>BottomWear</h3>
+                <ul>
+                  <li>Jeans</li>
+                  <li>Formal Trouser</li>
+                  <li>Casual Trouser</li>
+                  <li>Shorts</li>
+                  <li>TrackPants & Trousers</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Innerwear & SleepWear</h3>
+                <ul>
+                  <li>Brief & Thunks</li>
+                  <li>Boxers</li>
+                  <li>Vests</li>
+                  <li>SleepWear & LongeWear</li>
+                  <li>Thermals</li>
+                </ul>
+              </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>Shoes</h3>
+                <ul>
+                  <li>Casual-Shoes</li>
+                  <li>Formal Shoes</li>
+                  <li>Sports Shoes</li>
+                  <li>Sneakers</li>
+                  <li>Sandals & Loaters </li>
+                  <li>Flip-Flops</li>
+                  <li>Socks</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Personal-Care & Groomings</h3>
+                <h3>Sunglasses & Frames</h3>
+                <h3>Watches</h3>
+              </div>
+            </div>
+            <div className="navbar-menu-mens-two">
+              <div>
+                <h3>Sports and Active-Wear</h3>
+                <ul>
+                  <li>Casual-Shoes</li>
+                  <li>Formal Shoes</li>
+                  <li>Sports Shoes</li>
+                  <li>Sneakers</li>
+                  <li>Sandals & Loaters </li>
+                  <li>Flip-Flops</li>
+                  <li>Jackets & SweatShirts</li>
+                  <li>Sports-Activewear</li>
+                  <li>Swimwear</li>
+                </ul>
+              </div>
+              <div>
+                <h3>Gadgets</h3>
+                <ul>
+                 <li>Smart Wearable</li>
+                  <li>Fitness Gadgets</li>
+                  <li>HeadPhones</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>Studio
+           <div className="navbar-menu-studio">
+           <img height="40%" width="40%"
+           src="https://constant.myntassets.com/web/assets/img/sudio-nav-banner.png"/>
+           </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
